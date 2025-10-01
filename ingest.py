b4b6d1d537d1d26000afa6d4c58f72838ce3452d
ian.andriani <ian.andriani@bridge.ufsc.br>
2025-09-30 23:27:34 -0300

    Adicionando comentários, usa a identicação padrão do modelo e normaliza os embeddings

--- ingest.py
+++ ingest.py
@@ -4,7 +4,7 @@ import torch
 import os
 import textwrap
 
-# Carrega o modelo com suporte ao código remoto
+# Carrega o modelo de embeddings com suporte ao código remoto
 model = SentenceTransformer(
     "BAAI/bge-m3",
     trust_remote_code=True
@@ -18,6 +18,8 @@ if torch.cuda.is_available():
 else:
     print("⚠️  Rodando na CPU")
 
+
+# Conecta ao Qdrant + define o nome da coleção
 qdrant = QdrantClient("localhost", port=6333)
 collection_name = "rpg_lore_bge_m3"
 
@@ -26,8 +28,8 @@ if not qdrant.collection_exists(collection_name):
     qdrant.create_collection(
         collection_name=collection_name,
         vectors_config=models.VectorParams(
-            size=1024,
-            distance=models.Distance.COSINE
+            size=1024, # Dimensão do Embedding
+            distance=models.Distance.COSINE # Métrica de similaridade usada
         )
     )
 
@@ -35,9 +37,10 @@ if not qdrant.collection_exists(collection_name):
 def split_text(text, max_len=600):
     return textwrap.wrap(text, max_len, break_long_words=False, break_on_hyphens=False)
 
+# Contador de IDs
 point_id = 1
 
-# Lê todos os arquivos .txt dentro de docs/
+# Lê todos os arquivos .txt dentro de docs/ e indexa
 for file_name in os.listdir("docs"):
     if not file_name.endswith(".txt"):
         continue
@@ -47,9 +50,10 @@ for file_name in os.listdir("docs"):
         content = file.read()
         chunks = split_text(content)
 
+        # Lê o conteúdo e quebra em chunks
         for chunk in chunks:
-            prompt_chunk = "Representacao para recuperacao: " + chunk
-            vector = model.encode(prompt_chunk).tolist()
+            prompt_chunk = "Represent this passage for retrieving relevant documents: " + chunk
+            vector = model.encode(prompt_chunk,  normalize_embeddings=True).tolist()
 
             qdrant.upsert(
                 collection_name=collection_name,
